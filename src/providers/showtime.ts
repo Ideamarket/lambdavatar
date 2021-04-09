4de32bad699f0010e7384720ba032152f1b57e50
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2021-04-09 20:58:42 +0200

    chore: minor fixes

--- src/providers/showtime.ts
+++ src/providers/showtime.ts
@@ -4,7 +4,7 @@ import * as cheerio from 'cheerio'
 export default async function run(username: string): Promise<any> {
   const { body } = await got(`https://tryshowtime.com/${username}`)
   const $ = cheerio.load(body)
-  const res = JSON.parse($('script[id="__NEXT_DATA__"]').html())
+  const res = JSON.parse($('script[id="__NEXT_DATA__"]').html() as string)
   if (
     !res ||
     !res.props ||
